d4b82aa8e189f5ec01c9865844e9cf1ac5df922e
a19guilagbon <95306158+a19guilagbon@users.noreply.github.com>
2022-02-11 18:55:15 +0100

    PowerShell - GitHub Classroom - 1_GuillemLago

--- 09.ps1
+++ 09.ps1
@@ -1,7 +1,7 @@
 #si dos numeros passats per parametre a l'script son iguals mostra OK. Si son diferents mostra KO
-if ($var1 -eq $var2){
-    "OK"
+if ($var1[0] -eq $var2[1]){
+    Write-Output "OK"
 }
 else{
-    "KO"
+    Write-Output "KO"
 }
